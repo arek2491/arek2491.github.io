5d6bab3634c338feed5a5e7b8bfd736551c6722d
arek2491 <arkadiusz.matysiak@gmail.com>
2020-03-18 00:44:25 +0100

    Uaktualnienie aplikacji:

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   const apiRoot = 'http://damp-beyond-63121.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://damp-beyond-63121.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'http://damp-beyond-63121.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
